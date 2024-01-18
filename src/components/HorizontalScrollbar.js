9a3ec4c7618e3ad18166dbc9b911f2d6d05a4cd2
Jean Vargas <jeanvl.45@gmail.com>
2024-01-18 18:27:43 -0500

    23

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -1,30 +1,7 @@
-import React, { useContext } from 'react';
-import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
-import { Box, Grid, Typography } from '@mui/material';
-
+import React from 'react';
+import { Box, Grid } from '@mui/material';
 import EjerciciosCard from './EjerciciosCard';
 import BodyPart from './BodyPart';
-import RightArrowIcon from '../assets/icons/right-arrow.png';
-import LeftArrowIcon from '../assets/icons/left-arrow.png';
-
-const LeftArrow = () => {
-  const { scrollPrev } = useContext(VisibilityContext);
-
-  return (
-    <Typography onClick={() => scrollPrev()} className="right-arrow">
-      <img src={LeftArrowIcon} alt="right-arrow" />
-    </Typography>
-  );
-};
-
-const RightArrow = () => {
-  const { scrollNext } = useContext(VisibilityContext);
-  return (
-    <Typography   onClick={() => scrollNext()} className="left-arrow">
-      <img src={RightArrowIcon} alt="right-arrow" />
-    </Typography>
-  );
-};
 
 
 const HorizontalScrollbar = ({ data, bodyParts, setBodyPart, bodyPart }) => {
